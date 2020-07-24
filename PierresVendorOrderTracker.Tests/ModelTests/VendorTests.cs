c2273c8665d56c5bce3b5b75f2bbc59d2a7878d7
Tyson Lackey <lackeyt90@gmail.com>
2020-07-24 09:01:23 -0700

    initialize file tree / folders

---
+++ PierresVendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresVendorOrderTracker.Models;
+using System;
+
+namespace PierresVendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests //: IDisposable
+  {
+
+    // public void Dispose()
+    // {
+    //   Item.ClearAll();
+    // }
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      // Item newItem = new Item("test");
+      // Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+  }
+}
---
+++ PierresVendorOrderTracker/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PierresVendorOrderTracker.Models
+{
+  class Program
+  {
+    static void Main()
+    {
+      
+    }
+  }
+}
