8a614826b99d66b2296f4e1fe9b77333b5d78ee8
Tyson Lackey <lackeyt90@gmail.com>
2020-07-24 11:43:26 -0700

    update cshtml for vendor views and associated controller

--- PierresVendorOrderTracker/Controllers/VendorController.cs
+++ PierresVendorOrderTracker/Controllers/VendorController.cs
@@ -15,5 +15,41 @@ namespace PierresVendorOrderTracker.Controllers
       return View(allVendors);
     }
 
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDesc)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDesc);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string,object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.VendorOrders;
+      model.Add("vendor", selectedVendor);
+      model.Add("order", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDesc, int orderPrice, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string,object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDesc, orderPrice, orderDate);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.VendorOrders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
