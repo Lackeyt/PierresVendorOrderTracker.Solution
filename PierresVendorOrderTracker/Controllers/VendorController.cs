ded57d77f980c02e333a67b122f9b81be543d4e6
Tyson Lackey <lackeyt90@gmail.com>
2020-07-24 09:14:47 -0700

    add initial Controller files

---
+++ PierresVendorOrderTracker/Controllers/VendorController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PierresVendorOrderTracker.Controllers
+{
+    public class VendorController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
