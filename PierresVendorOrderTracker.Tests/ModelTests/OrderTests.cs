9c024f8d7454018bf24415ec0eb3b66a9b6231f5
Tyson Lackey <lackeyt90@gmail.com>
2020-07-24 10:48:48 -0700

    green light return Title test

--- PierresVendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ PierresVendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -20,5 +20,13 @@ namespace PierresVendorOrderTracker.Tests
       Order newOrder = new Order("testTitle", "TestDesc", 10, "testDate");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    public void GetOrderTitle_ReturnsTitle_String()
+    {
+      string testTitle =  "Test Title";
+      Order newOrder = new Order(testTitle, "testDescription", 10, "testDate");
+      string result = newOrder.Title;
+      Assert.AreEqual(testTitle, result);
+    }
   }
 }
